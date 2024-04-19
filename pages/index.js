80b29cd6145287d6ced3e244e0e5399aa8a66b88
tarek alhuseyin <you@example.com>
2024-04-19 22:31:46 +0300

    padding

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`flex  items-center justify-between `}>
-      <div className="z-10 flex bg-black w-full items-center justify-center ">
-        <div className=" left-0 flex h-96 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main
+      className={`flex  items-center justify-between lg:py-24 py-44 h-screen `}
+    >
+      <div className="z-10 flex  w-full h-full items-center justify-center ">
+        <div className=" left-0 flex h-full w-full items-center relative justify-center ">
           <Image
             src="/baner.JPG"
             alt="Vercel Logo"
-            className="dark:invert w-full h-full"
+            className="dark:invert w-full h-full absolute "
             fill
             priority
           />
