9f5d3fbd1e4f3359452722e6753b09dfe4369802
tarek alhuseyin <you@example.com>
2024-04-19 22:52:22 +0300

    img logo

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex  items-center justify-between lg:py-24 py-44 h-screen `}
+      className={`flex  items-center justify-between lg:py-24 py-44 h-screen  `}
     >
       <div className="z-10 flex  w-full h-full items-center justify-center ">
         <div className=" left-0 flex h-full w-full items-center relative justify-center ">
           <Image
-            src="/baner.JPG"
+            src="/banner.png"
             alt="Vercel Logo"
             className="dark:invert w-full h-full absolute "
             fill
