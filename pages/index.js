357b7d410fb1c08cad47030f988a1adb3125c557
tarek alhuseyin <you@example.com>
2024-04-19 22:21:26 +0300

    justyfy center

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className={`flex  items-center justify-between `}>
+      <div className="z-10 flex bg-black w-full items-center justify-center ">
+        <div className=" left-0 flex h-96 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Image
             src="/baner.JPG"
             alt="Vercel Logo"
