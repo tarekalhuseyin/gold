3ebf7ee508d89616e1846702802c5fa40de365b8
tarek alhuseyin <you@example.com>
2024-04-19 22:11:51 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <Image
+            src="/baner.JPG"
+            alt="Vercel Logo"
+            className="dark:invert w-full h-full"
+            fill
+            priority
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
